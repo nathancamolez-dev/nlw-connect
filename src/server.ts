f22ee5706438ca8fa383629c5e86731fa08c9058
nathancamolez-dev <nathan.camolez@unesp.br>
2025-02-17 22:03:08 -0300

    feat: Setup of fastify
    The first configurations of fastify using cors,zod for verification of
    types, typed-zod for controlling output and input types, swagger for
    documention and swagger ui to see on /docs

---
+++ src/server.ts
@@ -0,0 +1,42 @@
+import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import fastify from 'fastify'
+import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { env } from './env'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: true,
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'nlw-connect',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`HTTP server running on http://localhost:${env.PORT}`)
+  })
